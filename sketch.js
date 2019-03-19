29489690aa6f489be65da213615b0e40def4385d
Zayatsoff <47332155+Zayatsoff@users.noreply.github.com>
2019-03-18 23:30:12 -0400

    Continued adding

--- sketch.js
+++ sketch.js
@@ -4,7 +4,8 @@
 // Session 2: Array of Particles, multiple forces
 
 var person;
-var obstc = [];
+
+
 function setup() {
   createCanvas(640, 360);
   person = new Person();
@@ -21,16 +22,28 @@ function keyPressed() {
   }
 
 }
-
+var x = 400;
 function draw() {
   background(51);
 
-  var gravity = createVector(0, 0.1);
+  var gravity = createVector(0.005, 0.15);
   person.applyForce(gravity);
 
-  translate(-person.pos.x, 0);
+  translate(-person.pos.x +20, 0);
   person.update();
   person.edges();
   person.display();
+  fill(255, 0, 100);
+  rect(x, height - 50, 30, 50);
+  rect(x - 100, height - 50, 30, 50);
+  rect(x + 100, height - 50, 40, 50);
+  rect(x + 334, height - 50, 20, 50);
+  rect(x + 564, height - 50, 30, 50);
+  rect(x + 775, height - 50, 40, 50);
+  rect(x + 986, height - 50, 40, 50);
+  rect(x + 1097, height - 50, 50, 50);
+  rect(x + 1308, height - 50, 20, 50);
+  rect(x + 1687, height - 50, 30, 50);
 
+  rect
 }
