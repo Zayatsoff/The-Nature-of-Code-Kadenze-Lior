63c3f1ea158f926cddd2035aa510e6e0fcb49485
Zayatsoff <47332155+Zayatsoff@users.noreply.github.com>
2019-03-23 16:44:09 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,11 +4,13 @@
 // Session 2: Array of Particles, multiple forces
 
 var person;
-
+var ticks = 0;
 function preload() {
-  img = loadImage('https://i.imgur.com/EIGOHAU.png');
+  img = loadImage('https://i.imgur.com/dpqfSAb.png');
+  imgObs = loadImage('https://i.imgur.com/2zlFCEY.png');
   //theFont = loadFont('libraries\theFont\GermaniaOne-Regular.ttf');
 }
+
 function setup() {
 
   createCanvas(640, 360);
@@ -30,22 +32,49 @@ function draw() {
   //textFont(theFont);
   fill(123, 204, 189);
   text("Welcome to the..", 20, 60);
-textSize(40);
+  textSize(40);
   fill(157, 192, 249);
-  text("Neverendng-non-collision game!!", 20, 100);
-image(img, -100, 100)
+  text("Neverending-non-collision game!!", 20, 100);
+  image(img, 400, 100, 250, 250);
+  image(imgObs, 0, 130, 200, 200);
+  angleMode(DEGREES)
+  push();
+  rotate(10);
+  noStroke();
+  fill(87, 92, 96);
+  textSize(20);
+  text("(Press 'Space' to start!!)", 250, 170);
+
   if (key == ' ') {
 
 
+      pop();
+      background(51);
+      var gravity = createVector(0.005, 0.180);
+      person.applyForce(gravity);
+      translate(-person.pos.x + 20, 0);
+      person.update();
+      person.edges();
+      person.display();
+      Obstacle();
+      noStroke();
+      fill(87, 92, 96);
+      textSize(20);
+      text("Press 'Space' to jump", 250, 170);
+      text("You can press any key to pause", 800, 170);
+      text("Sadly, I haven't implemented collisions yet", 1500, 170);
+      text("Made by Lior, so there's that", 3000, 170);
+       ticks++;
+       console.log(ticks);
+       if (ticks > 2000) {
+         translate(0, 0);
+         background(43, 47, 51);
+         fill(87, 92, 96);
+         textSize(20);
+         text("Thanks for playing!\n You've played for about 30 sec according to my calculations.", person.pos.x, 170);
 
-    background(51);
-    var gravity = createVector(0.005, 0.180);
-    person.applyForce(gravity);
-    translate(-person.pos.x + 20, 0);
-    person.update();
-    person.edges();
-    person.display();
-    Obstacle();
+       }
+
 
   }
 
